2c134166a1891f71498488ad625bfa88b45c53e8
alejandropj <alejandroparrajimenez@gmail.com>
2023-11-17 13:01:02 +0100

    Grupo Componente

--- src/components/GrupoComponente.vue
+++ src/components/GrupoComponente.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>Grupos</h1>
+<v-card
+    class="mx-auto my-8"
+    max-width="344"
+    elevation="16"
+  >
+    <v-card-item>
+      <v-card-title>
+        Grupo {{index}}
+      </v-card-title>
+    </v-card-item>
+    <v-card-text v-for="alumno in alumnos" :key="alumno">
+      {{alumno.nombre}} {{alumno.apellidos}}
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-
-}
+  name: "GrupoComponent",
+  props:["alumnos","index"],
+  data() {
+    return {
+/*       alumnos: null,
+      index:null */
+    };
+  },
+  mounted() {
+    console.log("Montado");
+  },
+};
 </script>
 
 <style>
-
 </style>
