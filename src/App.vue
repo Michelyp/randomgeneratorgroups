be593ddd9044374485ffbd962f795e1e110d80f9
michelyp <michelypintom@gmail.com>
2023-11-17 13:19:06 +0100

    feat:modificar

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <v-app>
     <v-main>
       <WindowComponent/>
+      <audio-player></audio-player>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import WindowComponent from './components/WindowComponent.vue'
-
+import AudioPlayer from './components/AudioPlayerComponent.vue'
 export default {
   name: 'App',
 
   components: {
     WindowComponent,
+    AudioPlayer
   },
 
   data: () => ({
---
+++ src/components/AudioPlayerComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <audio ref="audio" :src="audioSource"></audio>
+    <button @click="togglePlayback">{{ isPlaying ? 'Pausar' : 'Reproducir' }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPlaying: false,
+      audioSource: "champion.mp3", 
+    };
+  },
+  methods: {
+    togglePlayback() {
+      const audio = this.$refs.audio;
+
+      if (this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+
+      this.isPlaying = !this.isPlaying;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
